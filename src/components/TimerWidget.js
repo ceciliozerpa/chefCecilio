b020bf4ef3a9130aadbad65254f934d4a97db80e
ceciliozerpa <85295606+ceciliozerpa@users.noreply.github.com>
2026-03-26 22:53:25 -0300

    Create TimerWidget.js

---
+++ src/components/TimerWidget.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function TimerWidget({ category, onClose }) {
+  // ... (todo el código de la función TimerWidget que ya tienes)
+}
